4d36ab07e48beb952cfa41992894b9388f3a380c
moisesjbc <moisesjbc@gmail.com>
2015-10-01 11:28:01 +0100

    Added prefab LODPlane.

---
+++ UnityCardboardProject/Assets/Scripts/LODPlane.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LODPlane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("LOD Plane created");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
