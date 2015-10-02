45078f2dd8d95d93a12e820d1b95531e4966852c
moisesjbc <moisesjbc@gmail.com>
2015-10-02 08:17:15 +0100

    Plane textures are downloaded in a no bloking way.

--- UnityCardboardProject/Assets/Scripts/LODPlane.cs
+++ UnityCardboardProject/Assets/Scripts/LODPlane.cs
@@ -7,40 +7,41 @@ public class LODPlane : MonoBehaviour {
 	private WWW www1;
 	private WWW www2;
 
+	private Renderer renderer;
+
 	// Use this for initialization
-	IEnumerator Start () {
+	void Start () {
 		Debug.Log ("LOD Plane created");
 
 		www0 = new WWW( "http://pixelkin.org/wp-content/uploads/2014/03/Metal-Gear-Solid-Color-Logo.jpg" );
 		www1 = new WWW( "https://upload.wikimedia.org/wikipedia/commons/7/7e/Metal_Gear_Solid_2_logo.png" );
 		www2 = new WWW( "https://upload.wikimedia.org/wikipedia/commons/c/c0/Metal_Gear_Solid_3_logo.png" );
 
-		// Wait for download to complete
-		yield return www0;
-		yield return www1;
-		yield return www2;
-
-		yield return null;
+		renderer = GetComponent<Renderer>();
+		renderer.material.shader = Shader.Find("Sprites/Default");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Renderer renderer = GetComponent<Renderer>();
-		renderer.material.shader = Shader.Find("Sprites/Default");
-
 		float distance = Vector3.Distance (transform.position, Camera.main.transform.position);
 
 		if (distance < 9.0f) {
 			if( www2.isDone ){
 				renderer.material.mainTexture = www2.texture;
+			}else{
+				renderer.material.mainTexture = Texture2D.whiteTexture;
 			}
 		} else if (distance < 10.0f) {
 			if( www1.isDone ){
 				renderer.material.mainTexture = www1.texture;
+			}else{
+				renderer.material.mainTexture = Texture2D.whiteTexture;
 			}
 		} else {
 			if( www0.isDone ){
 				renderer.material.mainTexture = www0.texture;
+			}else{
+				renderer.material.mainTexture = Texture2D.whiteTexture;
 			}
 		}
 	}
