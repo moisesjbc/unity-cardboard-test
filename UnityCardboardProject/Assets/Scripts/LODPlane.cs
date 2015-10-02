4288f757fb4d19511a7f584679f5b08ba7d613bd
moisesjbc <moisesjbc@gmail.com>
2015-10-02 09:43:53 +0100

    Fixed bug causing Android to reboot.

--- UnityCardboardProject/Assets/Scripts/LODPlane.cs
+++ UnityCardboardProject/Assets/Scripts/LODPlane.cs
@@ -2,47 +2,41 @@
 using System.Collections;
 
 public class LODPlane : MonoBehaviour {
-
-	private WWW www0;
-	private WWW www1;
-	private WWW www2;
-
-	private Renderer renderer;
+	private Texture2D[] textures;
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("LOD Plane created");
+		
+		GetComponent<Renderer>().material.shader = Shader.Find("Sprites/Default");
 
-		www0 = new WWW( "http://pixelkin.org/wp-content/uploads/2014/03/Metal-Gear-Solid-Color-Logo.jpg" );
-		www1 = new WWW( "https://upload.wikimedia.org/wikipedia/commons/7/7e/Metal_Gear_Solid_2_logo.png" );
-		www2 = new WWW( "https://upload.wikimedia.org/wikipedia/commons/c/c0/Metal_Gear_Solid_3_logo.png" );
+		textures = new Texture2D[3];
+		for( int i = 0; i < textures.Length; i++) {
+			textures[i] = Texture2D.whiteTexture;
+		}
 
-		renderer = GetComponent<Renderer>();
-		renderer.material.shader = Shader.Find("Sprites/Default");
+		StartCoroutine (LoadImage ("http://pixelkin.org/wp-content/uploads/2014/03/Metal-Gear-Solid-Color-Logo.jpg", 0));
+		StartCoroutine (LoadImage ("https://upload.wikimedia.org/wikipedia/commons/7/7e/Metal_Gear_Solid_2_logo.png", 1));
+		StartCoroutine (LoadImage ("https://upload.wikimedia.org/wikipedia/commons/c/c0/Metal_Gear_Solid_3_logo.png", 2));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float distance = Vector3.Distance (transform.position, Camera.main.transform.position);
-
+		float distance = 
+			Vector3.Distance (transform.position, Camera.main.transform.position);
+		
 		if (distance < 9.0f) {
-			if( www2.isDone ){
-				renderer.material.mainTexture = www2.texture;
-			}else{
-				renderer.material.mainTexture = Texture2D.whiteTexture;
-			}
+			GetComponent<Renderer>().material.mainTexture = textures[2];
 		} else if (distance < 10.0f) {
-			if( www1.isDone ){
-				renderer.material.mainTexture = www1.texture;
-			}else{
-				renderer.material.mainTexture = Texture2D.whiteTexture;
-			}
+			GetComponent<Renderer>().material.mainTexture = textures[1];
 		} else {
-			if( www0.isDone ){
-				renderer.material.mainTexture = www0.texture;
-			}else{
-				renderer.material.mainTexture = Texture2D.whiteTexture;
-			}
+			GetComponent<Renderer>().material.mainTexture = textures[0];
 		}
+	}
+
+	private IEnumerator LoadImage( string url, int textureIndex ) {
+		WWW www = new WWW( url );
+		yield return www;
+		textures[textureIndex] = www.texture;
 	}
 }
